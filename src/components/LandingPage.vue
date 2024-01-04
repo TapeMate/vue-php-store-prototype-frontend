3278a4d4cf4189ea6e18758b2287c91a8b3eac0d
TapeMate <admin@tapemate.onmicrosoft.com>
2024-01-04 13:43:52 +0100

    init

---
+++ src/components/LandingPage.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <h5>Landing Page</h5>
+    <br />
+    <SignupForm />
+  </div>
+</template>
+
+<script>
+import SignupForm from "@/components/SignupForm.vue";
+
+export default {
+  name: "LandingPage",
+  components: {
+    SignupForm,
+  },
+};
+</script>
+
+<style></style>
---
+++ src/components/SignupForm.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="index-signup">
+    <h4>SIGN UP</h4>
+    <p>Don't have an account yet? Sign up here!</p>
+    <form action="includes/signup.inc.php" method="post">
+      <input type="text" name="uid" placeholder="Username" />
+      <input type="password" name="pwd" placeholder="Password" />
+      <input type="password" name="pwdRepeat" placeholder="Repeat Password" />
+      <input type="text" name="email" placeholder="E-Mail" />
+      <br />
+      <button type="submit" name="submit">SIGN UP</button>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "SignupForm",
+};
+</script>
