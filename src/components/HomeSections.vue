3df8c4641b3540a3b69112770ac81ea6ca432249
TapeMate <admin@tapemate.onmicrosoft.com>
2024-01-04 18:56:47 +0100

    implemented basic home section layout.

--- src/components/HomeSections.vue
+++ src/components/HomeSections.vue
@@ -1,25 +1,26 @@
 <template>
-  <div class="home-section-container">
-    <section>
-      <div class="section-content-container">
-        <img src="" alt="" />
-      </div>
-      <div class="section-content-container">
-        <h5>Header</h5>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis
-          accusamus officia illum, dolores exercitationem nisi ut, ipsum labore
-          veritatis ex doloremque accusantium hic magni tempora totam recusandae
-          consequuntur repellat aut.
-        </p>
-      </div>
-    </section>
-  </div>
-  <div class="home-section-container">
-    <section>
-      <div class="section-signup-container">SIGNUP FORM HERE</div>
-      <div class="section-login-container">LOGIN FORM HERE</div>
-    </section>
+  <div class="home-section-wrapper">
+    <div class="section-container">
+      <section>
+        <div class="section-content-container">
+          <img src="https://dummyimage.com/300x200/000/fff" alt="" />
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis
+            accusamus officia illum, dolores exercitationem.
+          </p>
+        </div>
+        <div class="section-content-container">
+          <h5>We make professional Equipment.</h5>
+          <button>FIND OUR PRODUCTS HERE</button>
+        </div>
+      </section>
+    </div>
+    <div class="section-container">
+      <section>
+        <div class="section-signup-container">SIGNUP FORM HERE</div>
+        <div class="section-login-container">LOGIN FORM HERE</div>
+      </section>
+    </div>
   </div>
 </template>
 
