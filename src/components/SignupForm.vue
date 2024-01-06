ef9892eefee55ccb8871da95f048af6de0ef86e4
TapeMate <admin@tapemate.onmicrosoft.com>
2024-01-06 15:55:04 +0100

    embedded simple error handling to reload the page.

--- src/components/SignupForm.vue
+++ src/components/SignupForm.vue
@@ -2,11 +2,31 @@
   <div class="index-signup">
     <h4>SIGN UP</h4>
     <p>Don't have an account yet? Sign up here!</p>
-    <form method="post">
-      <input type="text" name="uid" placeholder="Username" />
-      <input type="password" name="pwd" placeholder="Password" />
-      <input type="password" name="pwdRepeat" placeholder="Repeat Password" />
-      <input type="text" name="email" placeholder="E-Mail" />
+    <form @submit.prevent="submitSignup" method="post">
+      <input
+        v-model="signupData.uid"
+        type="text"
+        name="uid"
+        placeholder="Username"
+      />
+      <input
+        v-model="signupData.pwd"
+        type="password"
+        name="pwd"
+        placeholder="Password"
+      />
+      <input
+        v-model="signupData.pwdRepeat"
+        type="password"
+        name="pwdRepeat"
+        placeholder="Repeat Password"
+      />
+      <input
+        v-model="signupData.email"
+        type="text"
+        name="email"
+        placeholder="E-Mail"
+      />
       <br />
       <button type="submit" name="submit">SIGN UP</button>
     </form>
@@ -16,5 +36,60 @@
 <script>
 export default {
   name: "SignupForm",
+  data() {
+    return {
+      signupData: {
+        uid: "",
+        pwd: "",
+        pwdRepeat: "",
+        email: "",
+      },
+    };
+  },
+  methods: {
+    submitSignup() {
+      console.log(this.signupData);
+      fetch(
+        "http://localhost/vue-php-store-prototype-backend/api/signup.api.php",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(this.signupData),
+        }
+      )
+        .then((response) => {
+          // checks response property if is false and throws error to get handled
+          if (!response.ok) {
+            throw new Error(
+              "Network response was not ok " + response.statusText
+            );
+          }
+          return response.json();
+        })
+        .then((data) => {
+          // check for application errors
+          if (data.error) {
+            console.error("Application Error:", data.error);
+            // Handle application-level error
+            // e.g., show an error message to the user
+          } else {
+            console.log("Success:", data);
+            // Handle success
+          }
+        })
+        .catch((error) => {
+          console.error("Network Error:", error);
+          // Handle network error
+          // e.g., show a network error message
+          this.reloadOnError();
+        });
+    },
+    reloadOnError() {
+      console.log("reloading the page");
+      window.location.reload();
+    },
+  },
 };
 </script>
